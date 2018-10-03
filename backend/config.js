d6b3c8576ffa09516badfbecd53c0ed211bfe3f9
kinsho <kinsho@gmail.com>
2018-10-03 00:50:21 -0400

    Revert "Deploy from Forestry.io"
    This reverts commit 3050b34830283ded8fefc0cfc08d19e36c2fea47.

---
+++ backend/config.js
@@ -0,0 +1,12 @@
+// --------- CONFIGURATION ---------
+
+var config =
+{
+	// Database
+	DATABASE_URL: 'mongodb://kinsho:jA0Ouykwzxo6NBz5@owl-blog-shard-00-00-abujc.mongodb.net:27017,owl-blog-shard-00-01-abujc.mongodb.net:27017,owl-blog-shard-00-02-abujc.mongodb.net:27017/test?ssl=true&replicaSet=owl-blog-shard-0&authSource=admin&retryWrites=true',
+	DATABASE_NAME: 'owlBlog'
+};
+
+// --------- EXPORTS ---------
+
+module.exports = config;
